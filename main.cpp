3cc4720428cc2342ae06766c7cc96def7107ddca
Ahmed Mimo <91825697+5217193@users.noreply.github.com>
2021-12-01 19:28:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
 int main(){
+  char a=a;
+  cin>>a;
+  if (conversione (&: a)==false){
+    cout <<"errore"<<endl;
+  }else{
+    cout << a <<endl;
+  }
   return 0;
 }
