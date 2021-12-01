9cf7935a3024b06440e274a8e35d4d9a3873b761
Ahmed Mimo <91825697+5217193@users.noreply.github.com>
2021-12-01 19:35:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main(){
   char a=a;
   cin>>a;
-  if (conversione (&: a)==false){
+  if (conversione ( &: a)==false){
     cout <<"errore"<<endl;
   }else{
     cout << a <<endl;
   }
   
   return 0;
-}
+
