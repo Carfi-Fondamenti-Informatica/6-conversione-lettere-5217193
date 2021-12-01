753c336aaffb141df6e0c6aab590262579b24635
Ahmed Mimo <91825697+5217193@users.noreply.github.com>
2021-12-01 19:24:13 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+using namespace std;
+
+#include "lib.h"
+
+int main() {
+  char a=a;
+  cin>>a;
+  if (conversione (&: a)==false){
+    cout <<"errore"<<endl;
+  }else{
+    cout << a <<endl;
+  }
+  return 0;
